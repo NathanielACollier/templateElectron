425d0c28206c55eadad8d4f5cbba34ddaf43be64
Nathaniel Collier <nathaniel.collier@aecc.com>
2017-09-05 07:49:08 -0500

    Got a working example in place

--- main.ts
+++ main.ts
@@ -1,8 +1,34 @@
 import {app, BrowserWindow} from 'electron';
 
-let mainWindow: BrowserWindow = null;
+let mainWindow: Electron.BrowserWindow = null;
 
 app.on('ready',()=>{
-    mainWindow = new BrowserWindow({width: 800, height: 600});
-    
+    createWindow();
 });
+
+app.on('window-all-closed', ()=>{
+    if( process.platform !== 'darwin'){
+        app.quit();
+    }
+});
+
+app.on('activate', () =>{
+    if( mainWindow == null ){
+        createWindow();
+    }
+});
+
+// some electron typings bug prevents just process.on("<event name>")
+(process as NodeJS.EventEmitter).on('exit', (code) => {
+    console.log('exit');
+});
+
+function createWindow(){
+    mainWindow = new BrowserWindow({width: 800, height: 600});
+    mainWindow.loadURL("http://google.com");
+    mainWindow.webContents.openDevTools();
+
+    mainWindow.on('closed',()=>{
+        mainWindow = null;
+    });
+}
